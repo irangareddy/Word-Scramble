2dcbcfb00a18e4bcda76308d54679653c008dd88
irangareddy <itsmerr17@gmail.com>
2020-08-30 12:14:26 +0530

    Autogenerated Stuff

---
+++ Word Scramble/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Word Scramble
+//
+//  Created by RANGA REDDY NUKALA on 30/08/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Word Scramble/Word_ScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Word_ScrambleApp.swift
+//  Word Scramble
+//
+//  Created by RANGA REDDY NUKALA on 30/08/20.
+//
+
+import SwiftUI
+
+@main
+struct Word_ScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
